79bcd46318e58a106e8c0ac626636e751fe4509e
Robert Alexandre <robert.alexandrea7@gmail.com>
2023-04-05 12:46:54 -0300

    Adicionado ejs e express para rendererizar qr code no frontend

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
 const https = require("https");
+const express = require("express");
 
 const cert = fs.readFileSync(
   path.resolve(__dirname, `../certs/${process.env.GN_CERT}`)
@@ -20,46 +21,58 @@ const credentials = Buffer.from(
   `${process.env.GN_CLIENT_ID}:${process.env.GN_CLIENT_SECRET}`
 ).toString("base64");
 
-axios({
-  method: "POST",
-  url: `${process.env.GN_ENDPOINT}/oauth/token`,
-  headers: {
-    Authorization: `Basic ${credentials}`,
-    "Content-Type": "application/json",
-  },
-  httpsAgent: agent,
-  data: {
-    grant_type: "client_credentials",
-  },
-}).then((response) => {
-  const accessToken = response.data?.access_token;
-  const reqGN = axios.create({
-    baseURL: process.env.GN_ENDPOINT,
-    httpsAgent: agent,
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", "src/views");
+
+app.get("/", (req, res) => {
+  axios({
+    method: "POST",
+    url: `${process.env.GN_ENDPOINT}/oauth/token`,
     headers: {
-      Authorization: `Bearer ${accessToken}`,
+      Authorization: `Basic ${credentials}`,
       "Content-Type": "application/json",
     },
+    httpsAgent: agent,
+    data: {
+      grant_type: "client_credentials",
+    },
+  }).then((response) => {
+    const accessToken = response.data?.access_token;
+    const reqGN = axios.create({
+      baseURL: process.env.GN_ENDPOINT,
+      httpsAgent: agent,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    const dataCob = {
+      calendario: {
+        expiracao: 3600,
+      },
+      valor: {
+        original: "100.00",
+      },
+      chave: "12460f5a-63c9-41b0-905f-927f7da55cdb",
+      solicitacaoPagador: "Cobrança do LinkPed",
+    };
+
+    reqGN
+      .post("/v2/cob", dataCob)
+      .then((response) => res.send(response.data));
   });
 
-  const dataCob = {
-    calendario: {
-      expiracao: 3600,
-    },
-    valor: {
-      original: "100.00",
-    },
-    chave: "12460f5a-63c9-41b0-905f-927f7da55cdb",
-    solicitacaoPagador: "Cobrança do LinkPed",
-  };
-
-  reqGN.post("/v2/cob", dataCob).then((response) => console.log(response.data));
+  /*curl --request POST \
+    --url https://api-pix-h.gerencianet.com.br/oauth/token \
+    --header 'Authorization: Basic Q2xpZW50X0lkXzNlZDZmMDZmMzQ3ZWUxYjY1NWY2MmI2ZDI3OTdkYWFhODk1MTAzZDM6Q2xpZW50X1NlY3JldF8yMDllZTVjZWVkYWIzMDBhNzFmMzI2MTZmNjE4NzI5NzAyMmNmYWMw' \
+    --header 'Content-Type: application/json' \
+    --data '{
+    "grant_type": "client_credentials"
+  }'*/
 });
 
-/*curl --request POST \
-  --url https://api-pix-h.gerencianet.com.br/oauth/token \
-  --header 'Authorization: Basic Q2xpZW50X0lkXzNlZDZmMDZmMzQ3ZWUxYjY1NWY2MmI2ZDI3OTdkYWFhODk1MTAzZDM6Q2xpZW50X1NlY3JldF8yMDllZTVjZWVkYWIzMDBhNzFmMzI2MTZmNjE4NzI5NzAyMmNmYWMw' \
-  --header 'Content-Type: application/json' \
-  --data '{
-	"grant_type": "client_credentials"
-}'*/
+app.listen(8000, () => {
+  console.log("Rodando server");
+});
