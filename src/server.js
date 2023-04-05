2004c77e876f93ce9e344bf4d30b6bb82c9438f0
Robert Alexandre <robert.alexandrea7@gmail.com>
2023-04-05 12:41:46 -0300

    axios com config padrão; gerando cobrança

--- src/server.js
+++ src/server.js
@@ -1,37 +1,60 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
-const https = require('https');
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+const https = require("https");
 
 const cert = fs.readFileSync(
-    path.resolve(__dirname, `../certs/${process.env.GN_CERT}`)
+  path.resolve(__dirname, `../certs/${process.env.GN_CERT}`)
 );
 
 const agent = new https.Agent({
-    pfx: cert,
-    passphrase: ''
+  pfx: cert,
+  passphrase: "",
 });
 
 const credentials = Buffer.from(
-    `${process.env.GN_CLIENT_ID}: ${process.env.GN_CLIENT_SECRET}`
-).toString('base64');
+  `${process.env.GN_CLIENT_ID}:${process.env.GN_CLIENT_SECRET}`
+).toString("base64");
 
 axios({
-    method: 'POST',
-    url: `${process.env.GN_ENDPOINT}/oauth/token`,
-    headers: {
-        Authorization: `Basic ${credentials}`,
-        'Content-Type': 'application/json'
-    },
+  method: "POST",
+  url: `${process.env.GN_ENDPOINT}/oauth/token`,
+  headers: {
+    Authorization: `Basic ${credentials}`,
+    "Content-Type": "application/json",
+  },
+  httpsAgent: agent,
+  data: {
+    grant_type: "client_credentials",
+  },
+}).then((response) => {
+  const accessToken = response.data?.access_token;
+  const reqGN = axios.create({
+    baseURL: process.env.GN_ENDPOINT,
     httpsAgent: agent,
-    data: {
-        grant_type: 'client_credentials'
-    }
-}).then(console.log)
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  const dataCob = {
+    calendario: {
+      expiracao: 3600,
+    },
+    valor: {
+      original: "100.00",
+    },
+    chave: "12460f5a-63c9-41b0-905f-927f7da55cdb",
+    solicitacaoPagador: "Cobrança do LinkPed",
+  };
+
+  reqGN.post("/v2/cob", dataCob).then((response) => console.log(response.data));
+});
 
 /*curl --request POST \
   --url https://api-pix-h.gerencianet.com.br/oauth/token \
